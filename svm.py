3dfcf55be1e19005093c7bfee4175fd463eca930
negar mohamadhassan <nemote95@users.noreply.github.com>
2015-12-25 15:23:43 +0330

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Dec 23 12:44:33 2015
+
 @author: Negmo
 """
 from skimage import io
@@ -21,7 +23,7 @@ train_size=10
 stones=[]
 for typ in 'abcd':
     for i in range(0,train_size):
-        stones.append(load(r"\\dataset\%s" % typ,'%d.jpg' % i))#change the directory to your dataset directory
+        stones.append(load(r"C:\Users\Negmo\.spyder2-py3\dataset\%s" % typ,'%d.jpg' % i))
         
 #extract texture features using glcm
 xs=[]#array of dissimilarities
@@ -48,7 +50,7 @@ incorrect=[]
 correct=[]
 for typ in 'abcd':
     for i in range(train_size,37):
-        p_img=load(r"\\dataset\%s" % typ,'%d.jpg' % i)#change the directory to your dataset directory
+        p_img=load(r"C:\Users\Negmo\.spyder2-py3\dataset\%s" % typ,'%d.jpg' % i)
         p_glcm=greycomatrix(p_img, [5], [0], 256, symmetric=True, normed=True)
         p=[greycoprops(p_glcm, 'dissimilarity')[0, 0],greycoprops(p_glcm, 'correlation')[0, 0],greycoprops(p_glcm,'ASM')[0,0]]
         prediction=clf.predict([p])
